8ffcf02ab4a3db7e9bfe0e74b0202117c4962962
Ian Juma <ian.juma@yahoo.com>
2015-06-08 16:06:17 +0300

    models fix

--- products/models.py
+++ products/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from distributors.models import Distributor
 
 
+class Category(models.Model):
+    CATEGORIES = (
+        ('E', 'Airtime'),
+        ('G', 'General')
+    )
+    category = models.CharField(max_length=1, choices=CATEGORIES)
+    # product_id = models.OneToOneRel(Product, to=Product, field_name='product_id')
+
+
 class Product(models.Model):
     CATEGORIES = (
         ('E', 'Airtime'),
@@ -9,7 +18,8 @@ class Product(models.Model):
     )
     # id = models.AutoField(primary_key=True, auto_created=True)
     owner = models.ForeignKey(Distributor)
-    type = models.CharField(max_length=1, choices=CATEGORIES)
+    # type = models.CharField(max_length=1, choices=CATEGORIES)
+    category = models.ForeignKey(Category)  # many to one rel
     name = models.CharField(max_length=50, blank=False)
     quantity = models.PositiveIntegerField(default=0)
     units = models.CharField(max_length=20)
