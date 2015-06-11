d8766a5269b4514015b343ae6f441e7cf9ad22d9
Ian Juma <ian.juma@yahoo.com>
2015-06-11 16:50:22 +0300

    rm migrations

--- transactions/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import permissions
-
-
-class IsProductOwner(permissions.BasePermission):
-    def has_object_permission(self, request, view, transaction):
-        if request.user:
-            return transaction == request.user
-        return False
--- transactions/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-from views import transaction_list, transaction_detail
-
-urlpatterns = patterns(
-    '',
-    url(r'^transaction/$', transaction_list, name='transaction_list'),
-    url(r'^transaction/(?P<pk>[0-9]+)$', transaction_detail, name='transaction_detail'),
-)
