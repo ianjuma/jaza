e2d1de5c388dd4841b6a335a4991909ac5e3929b
Ian Juma <ian.juma@yahoo.com>
2015-06-24 20:00:52 +0300

    assets and dir

--- agents/models.py
+++ agents/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from products.models import Product
+from django.core.validators import RegexValidator
 
 
 class Agent(models.Model):
     name = models.CharField(max_length=255)
 
-    phone_number = models.CharField(max_length=32, unique=True)
+    phone_number = models.CharField(max_length=16, unique=True, validators=[RegexValidator(
+        regex=r'^\+?254?\d{9,16}$', message="""Phone number must be entered in
+        the format: '+254XXXXXXXXX'. Up to 16 digits allowed."""
+    )])
     pin = models.CharField(max_length=64, blank=False)
     products = models.ManyToManyField(Product)
 
