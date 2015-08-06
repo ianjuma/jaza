4f2c7ad58249d0bf41f5b825f9511e64261707ea
Ian Juma <ian.juma@yahoo.com>
2015-08-06 18:14:17 +0300

    agents model admin removed

--- agents/admin.py
+++ agents/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
-from agents.models import Agent
+#from django.contrib import admin
+#from agents.models import Agent
 
-admin.site.register(Agent, admin.ModelAdmin)
+# admin.site.register(Agent, admin.ModelAdmin)
