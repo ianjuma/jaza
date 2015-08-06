bffd7d31c2833e4cb188adf1f7dae98985e3e105
Ian Juma <ian.juma@yahoo.com>
2015-08-06 14:38:58 +0300

    updated: sleuth views and urls

--- utils/urls.py
+++ utils/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 
-from utils.views import CrunchView
+from utils.views import (CrunchView, SleuthAgentTopUpView,
+                         SleuthGetAgentBalanceView)
 
 urlpatterns = [
     url('^crunch/$', CrunchView.as_view()),
+    url('^sleuth/$', SleuthAgentTopUpView.as_view()),
+    url('^sleuth/$', SleuthGetAgentBalanceView.as_view())
 ]
--- utils/views.py
+++ utils/views.py
@@ -41,7 +41,36 @@ class CrunchView(views.APIView):
         return Response(response_data)
 
 
-class SleuthView(views.APIView):
+class SleuthAgentTopUpView(views.APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, format=None):
+        data = request.query_params
+        try:
+            agent_id = data['agentId']
+            product_id = data['productId']
+            user_id = data['userId']
+            amount = data['amount']
+
+            gateway = SleuthGateway()
+            response_data = gateway.top_up_agent(
+                agent_id=agent_id,
+                product_id=product_id,
+                user_id=user_id,
+                amount=amount
+            )
+        except SleuthGatewayException, e:
+            print "Caught exception when calling AT Gateway: " + str(e)
+            response_data = {}
+
+        except Exception, e:
+            print "Caught exception when calling AT Gateway: " + str(e)
+            response_data = {}
+
+        return response_data
+
+
+class SleuthGetAgentBalanceView(views.APIView):
     permission_classes = (permissions.IsAuthenticated,)
 
     def get(self, request, format=None):
