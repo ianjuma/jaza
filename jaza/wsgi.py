fb953b124457a1e977f632255d8f0fe4e2456414
africastalking <gikandi@gmail.com>
2015-09-07 12:07:53 +0300

    - Add morpheus directory to path so that we can run multiple wsgi applications on apache

--- jaza/wsgi.py
+++ jaza/wsgi.py
@@ -7,7 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+sys.path.append('/opt/domains/morpheus-web')
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rest.settings")
 
 from django.core.wsgi import get_wsgi_application
