d077b56c1ff7328d47d7648dac6424103a5c0a8d
Ian Juma <ian.juma@yahoo.com>
2015-12-03 12:30:10 +0300

    -fixed routing with angular and pushState

--- jaza/urls.py
+++ jaza/urls.py
@@ -11,8 +11,6 @@ router.register(r'accounts', UserViewSet)
 
 urlpatterns = patterns(
     '',
-    url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^accounts/login', LoginView.as_view(), name='login'),
     url(r'^api/v1/', include(router.urls)),
     url(r'^api/v1/', include('agents.urls')),
     url(r'^api/v1/', include('utils.urls')),
@@ -20,9 +18,9 @@ urlpatterns = patterns(
     url(r'^api/v1/', include('products.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^accounts/login', LoginView.as_view(), name='login'),
+    url(r'^.*$', IndexView.as_view(), name='index'),
 )
 handler404 = 'views.handle_page_not_found_404'
 
-# index route last - angular overwrite all endpoints - avoid 404?
 # TODO: fix app route url(r'^.*$', IndexView.as_view(), name='index')
-# TODO: cleaner routes with class based views
